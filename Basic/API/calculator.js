dc435d7b86bdedaf488ee8a1ef25527f24347357
Pitchakorn Thiprangsi <kokejohh@gmail.com>
2024-12-15 03:59:17 +0700

    calculator.js separate server

--- Basic/API/calculator.js
+++ Basic/API/calculator.js
@@ -1,9 +1,21 @@
 const express = require('express');
+const QueryString = require('qs');
+const { queryObjects } = require('v8');
 
 const app = express();
 
+// app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.get('/', (req, res) => {
     res.send('Hello World');
+});
+
+app.post('/', (req, res) => {
+    console.log(req.body);
+    const { num1, num2 } = req.body;
+    const result = Number(num1) + Number(num2);
+    res.send(`answer is ${result}`);
 });
 
 app.listen(3000, () => {
