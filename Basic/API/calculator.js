914de928add68167a55a044cc10f3f01c5f580b3
Pitchakorn Thiprangsi <kokejohh@gmail.com>
2024-12-15 16:32:24 +0700

    test for cors

--- Basic/API/calculator.js
+++ Basic/API/calculator.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const path = require('path');
+const cors = require('cors');
 
 const app = express();
-
+//app.use(cors({ origin: 'http://localhost:5500'}));
+//app.use(cors());
 // app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
